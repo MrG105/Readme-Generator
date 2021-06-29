959588386fdb3c9355c48f237877890efe840f6e
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-06-29 17:10:23 -0500

    added generateMarkdown.js functionality and license info

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Readme-Generator
+# <Yes>
+  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+  ![GitHub license](https://img.shields.io/badge/license-GNU-yellowgreen.svg)
+  ## Description
+  What was your motivation?
+    - Yesss
+  Why did you build this project?
+    - Yes
+  What problem does it solve?
+    - yes
+  What did you learn making this project?
+    - yessssss
+  
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - 
+* [License](#license)
+
+
+  ## Installation
+  The application will be invoked by using the following command:
+    node index.js
+
+  ## Usage
+      ![alt text](assets/images/screenshot.png)
+      
+  ## Credits
+  Contact me at: [email](yesyesyes "email")
+  Github can be located at: [Github](https://github.com/MrG105 "Github")
+  
+  ## License
+      Licensed under the GNU license
+  ---
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+      Licensed under the ${license} license`
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# <${data.name}>
+  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  What was your motivation?
+    - ${data.motivation}
+  Why did you build this project?
+    - ${data.reason}
+  What problem does it solve?
+    - ${data.problem}
+  What did you learn making this project?
+    - ${data.learn}
+  
+  ## Table of Contents 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - ${renderLicenseLink(data.license)}
 
-`;
+  ## Installation
+  The application will be invoked by using the following command:
+    ${data.install}
+
+  ## Usage
+      ![alt text](assets/images/screenshot.png)
+      
+  ## Credits
+  Contact me at: [email](${data.email} "email")
+  Github can be located at: [Github](https://github.com/${data.github} "Github")
+  
+  ${renderLicenseSection(data.license)}
+  ---`;
 }
 
 module.exports = generateMarkdown;
